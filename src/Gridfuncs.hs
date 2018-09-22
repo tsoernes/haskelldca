e2f6dccf92bbfbd7b5bd447645ff85dce09b49d8
torstein <t.soernes@gmail.com>
2018-09-22 16:21:50 +0200

    Improve comment formatting; implement executing actions on grid

--- src/Gridfuncs.hs
+++ src/Gridfuncs.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TypeOperators #-}
+{-#LANGUAGE NamedFieldPuns#-}
 {-# LANGUAGE FlexibleContexts #-}
 module Gridfuncs where
 
@@ -8,14 +9,14 @@ import Gridneighs
 import Base
 import qualified Prelude as P
 
--- One-hot map of channels in use at cell neighbors with distance of 2 or less
+-- | One-hot map of channels in use at cell neighbors with distance of 2 or less
 inuseMap :: Cell -> Grid -> GridCell
 inuseMap cell grid = P.foldl1 (zipWith (||)) (P.map (`sliceCell` grid) neighs)
   where
     neighs = getNeighs 2 cell False
 
 
--- One-hot array of eligible channels
+-- | One-hot array of eligible channels
 -- TODO Can this be one inside accelerate? foldl is not accelerate code
 eligibleMap :: Cell -> Grid -> GridCell
 eligibleMap cell grid = map not notEmap
@@ -26,13 +27,13 @@ eligibleMap cell grid = map not notEmap
     bitor :: GridCell -> Cell -> GridCell
     bitor gc cell' = zipWith (||) gc (sliceCell cell' grid)
 
--- Get the allocation map of a particular cell
+-- | Get the allocation map of a particular cell
 sliceCell :: Cell -> Grid -> GridCell
 sliceCell (r, c) grid = slice grid (constant (Z :. r :. c :. All))
 
 
--- Return the eligible channels for the given cell. A channel is eligible if it is free at
--- the cell and all of its neighbors with distance of 2 or less.
+-- | Return the eligible channels for the given cell. A channel is eligible if it is free at
+-- | the cell and all of its neighbors with distance of 2 or less.
 eligibleChs :: Cell -> Grid -> Chs
 eligibleChs cell grid = elig
   where
@@ -61,9 +62,9 @@ eligibleChs cell grid = elig
 --         yy = unlift xx
 --         tviols = the viols :: Exp Bool
 
--- Given a grid 'grid' and a set of actions, the latter specified by a cell, an event type
--- and a list of channels, return the grids ('afterstates') that would result from
--- executing each of the actions on 'grid'
+-- | Given a grid 'grid' and a set of actions, the latter specified by a cell, an event type
+-- | and a list of channels, return the grids ('afterstates') that would result from
+-- | executing each of the actions on 'grid'
 afterstates :: Grid -> Cell -> EType -> Chs -> Grids
 afterstates grid (r, c) etype chs = permute const grids idxmap vals
   where
@@ -72,11 +73,11 @@ afterstates grid (r, c) etype chs = permute const grids idxmap vals
     idxmap :: Exp DIM1 -> Exp DIM4
     idxmap nsh = index4 (unindex1 nsh) (lift r) (lift c) (lift $ chs ! nsh)
 
--- A feature representation (frep for short) of the grid. The frep is of the
--- same spatial dimension as the grid. For a given cell, the first 'CHANNELS' features
--- specifies how many times each of the channels is in used within a 4-cell radius,
--- not including the cell itself. An additional feature counts the number of eligible
--- channels in that cell.
+-- | A feature representation (frep for short) of the grid. The frep is of the
+-- | same spatial dimension as the grid. For a given cell, the first 'CHANNELS' features
+-- | specifies how many times each of the channels is in used within a 4-cell radius,
+-- | not including the cell itself. An additional feature counts the number of eligible
+-- | channels in that cell.
 featureRep :: Grid -> Frep
 featureRep grid = P.foldl fn zeros gridIdxs
   where
@@ -106,3 +107,11 @@ index4
     -> Exp i
     -> Exp (Z :. i :. i :. i :. i)
 index4 k j i l = lift (Z :. k :. j :. i :. l)
+
+
+-- | Switch bit at given cell off for END events; on for NEW events
+executeAction :: Event -> Ch -> Grid -> Grid
+executeAction Event{etype, cell=(r, c)} toCh grid =
+    let val = unit $ lift $ etype P./= END
+        setIdx _ = constant (Z :. r :. c :. toCh)
+    in permute const grid setIdx val
