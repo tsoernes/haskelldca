ebc5679ab8df62f7c8cf67b0d939a1e033753540
torstein <t.soernes@gmail.com>
2018-09-22 16:24:08 +0200

    Parse settings from command line

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,23 @@
 module Main where
+
 import Gridneighs
 import Gridfuncs
+import Control.Monad.Reader
+import Base
+import Opt
+import Options.Applicative
 
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = runReader printAlphas =<< execParser opts
+  where
+    opts = info (getOpts <**> helper)
+      ( fullDesc
+     <> header "DCA - Dynamic Channel Allocation by Reinforcement Learning" )
+
+printAlphas :: Reader Opt (IO())
+printAlphas = do
+  alpha <- asks alphaNet
+  return $ print alpha
+
+simulate :: Reader Opt (Double, Double)
+simulate = return (0.0, 0.0)
---
+++ src/Opt.hs
@@ -0,0 +1,74 @@
+module Opt where
+import Options.Applicative
+import Data.Semigroup ((<>))
+
+data Opt = Opt
+  { callDurNew      :: Double
+  , callDurHoff      :: Double
+  , callRate      :: Double
+  , hoffProb      :: Double
+  , nEvents      :: Int
+  , logIter      :: Int
+  , alphaNet      :: Double
+  , alphaAvg      :: Double
+  , alphaGrad      :: Double
+  , verifyReuseConstraint :: Bool }
+  deriving Show
+
+getOpts :: Parser Opt
+getOpts = Opt
+      <$> option auto
+          ( long "call_dur"
+         <> metavar "MINUTES"
+         <> showDefault
+         <> value 3.0
+         <> help "Call duration for new calls" )
+      <*> option auto
+          ( long "call_dur_hoff"
+         <> metavar "MINUTES"
+         <> showDefault
+         <> value 3.0
+         <> help "Call duration for new calls" )
+      <*> option auto
+          ( long "call_rate"
+         <> metavar "CALLS_PER_HOUR"
+         <> showDefault
+         <> value 200.0
+         <> help "Call arrival rate" )
+      <*> option auto
+          ( long "hoff_prob"
+         <> metavar "PROBABILITY"
+         <> showDefault
+         <> value 0.0
+         <> help "Hand-off probability" )
+      <*> option auto
+          ( long "n_events"
+         <> metavar "N"
+         <> showDefault
+         <> value 10000
+         <> help "Simulation duration in number of processed events" )
+      <*> option auto
+          ( long "log_iter"
+         <> metavar "N"
+         <> showDefault
+         <> value 1000
+         <> help "How often to show call blocking probability and stats" )
+      <*> option auto
+          ( long "learning_rate"
+         <> metavar "alpha_net"
+         <> showDefault
+         <> value 2.52e-6)
+      <*> option auto
+          ( long "learning_rate_avg"
+         <> metavar "alpha_avg"
+         <> showDefault
+         <> value 0.06
+         <> help "" )
+      <*> option auto
+          ( long "learning_rate_grad"
+         <> metavar "alpha_grad"
+         <> showDefault
+         <> value 5e-6
+         <> help "" )
+      <*> switch ( long "verify_reuse_constraint" )
+
