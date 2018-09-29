65c073511cf61883b633dcef85f3d5862da67f84
torstein <t.soernes@gmail.com>
2018-09-29 15:01:14 +0200

    Use Monad constraints instead of specific instances.
    This makes it impossible to 'zoom' on state using lenses. Sub-state must
    henceforth be modified other ways, e.g. by combining State.modify
    with Lens.over.

--- src/EventGen.hs
+++ src/EventGen.hs
@@ -2,7 +2,10 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE Rank2Types #-}
 
 module EventGen
   ( EventGen
@@ -14,72 +17,143 @@ module EventGen
   , mkEventGen
   , pop
   , push
+  , statePart
+  , modifyPart
   ) where
 
-import           Base
-import           Control.Monad (forM_, foldM)
-import           Control.Monad.Reader (Reader, asks, lift)
-import           Control.Monad.State.Lazy (State, MonadState, StateT, get, put, modify', state, execStateT)
-import qualified Data.Heap as Heap
+import Base
+    ( Cell,
+      Ch,
+      EType(..),
+      Event(Event),
+      EventId,
+      EventKey(..),
+      evCell,
+      evEndCh,
+      gridIdxs )
+import Control.Lens
+    ( Lens', use, view, over, set, makeLenses, Zoom(zoom) )
+import Control.Monad ( foldM, forM_ )
+import Control.Monad.Reader ( MonadReader, asks )
+import Control.Monad.State.Lazy
+    ( MonadState(state), State, StateT, execStateT, gets, modify' )
+import qualified Data.Heap as Heap ( MinHeap, empty, insert, view )
 import qualified Data.Map.Strict as Map
-import           Data.Maybe (fromJust)
-import           Data.Random (MonadRandom, sampleState)
-import           Data.Random.Distribution.Exponential (exponential)
-import           Gridneighs (getNeighs)
-import           Control.Lens (makeLenses, use, zoom, _1, _2, view, set)
-import           Opt
-import           System.Random (StdGen, randomR)
+    ( Map, (!), adjust, delete, empty, insert )
+import Data.Maybe ( fromJust )
+import Data.RVar ( getRandomDouble, getRandomWord64, sampleRVar )
+import Data.Random ( MonadRandom, uniform )
+import Data.Random.Distribution.Exponential ( exponential )
+import Data.Random.Internal.TH ()
+import Data.Random.Source ( monadRandom )
+import Data.Word ( Word64 )
+import Gridneighs ( getNeighs )
+import Opt ( Opt(callDurHoff, callDurNew, callRate) )
+import System.Random.Mersenne.Pure64
+    ( PureMT, pureMT, randomDouble, randomWord64 )
+
 
 data EventGen = EventGen
   { _egId :: EventId -- Last used event ID
   , _queue :: Heap.MinHeap EventKey -- Min-heap of event-identifiers sorted on event times
   , _events :: Map.Map EventId Event -- Mapping from event IDs to event structs
   , _endIds :: Map.Map (Cell, Ch) EventId -- Mapping from cell-channel pairs to END event IDs
+  , _gen :: PureMT
   }
+
+mke :: Word64 -> EventGen
+mke = EventGen 0 Heap.empty Map.empty Map.empty . pureMT
 
 makeLenses ''EventGen
 
+useGen :: (MonadState EventGen m) => (PureMT -> (t, PureMT)) -> m t
+useGen thing = do
+  mt <-
+    gets (view gen) :: (MonadState EventGen m) =>
+                         m PureMT
+  let (ws, newMt) = thing mt
+  modify' (set gen newMt)
+  return ws
 
-mkEventGen :: StateT StdGen (Reader Opt) EventGen
-mkEventGen = do
-  gen <- get
-  let mke = EventGen 0 Heap.empty Map.empty Map.empty
-      addEvent :: () -> Cell -> StateT (StdGen, EventGen) (Reader Opt) ()
-      addEvent _ = generateNewEvent 0.0
-  (gen', eg) <- lift $ execStateT (foldM addEvent () gridIdxs) (gen, mke)
-  put gen'
-  return eg
+$(monadRandom
+    [d|
+  instance Monad m => MonadRandom (StateT EventGen m) where
+          getRandomWord64 = useGen randomWord64
+          getRandomDouble = useGen randomDouble
+  |])
 
+mkEventGen :: (MonadReader Opt m) => Word64 -> m EventGen
+mkEventGen seed = execStateT fn (mke seed)
+  where
+    fn ::
+         (Monad m, MonadRandom (StateT EventGen m), MonadReader Opt m)
+      => (StateT EventGen m) ()
+    fn = foldM (\_ -> generateNewEvent 0.0) () gridIdxs
+
+-- COMPILES, although you get:
+--  Illegal instance declaration for
+--    ‘MonadRandom (StateT EventGen m)’
+--    (All instance types must be of the form (T a1 ... an)
+--     where a1 ... an are *distinct type variables*,
+--     and each type variable appears at most once in the instance head.
+--     Use FlexibleInstances if you want to disable this.)
+--  In the instance declaration for ‘MonadRandom (StateT EventGen m_a1cSc)’ (haskell-dante)
 -- | Push an event into the event generator.
-push :: Double -> EType -> Cell -> Maybe Ch -> Maybe Cell -> State EventGen ()
+push ::
+     (MonadState EventGen m)
+  => Double
+  -> EType
+  -> Cell
+  -> Maybe Ch
+  -> Maybe Cell
+  -> m ()
 push time etype cell endCh hoffCell = do
-  eId <- use egId :: State EventGen Int
+  eId <- use egId
   let event = Event time etype cell endCh hoffCell
-  zoom events . modify' $ Map.insert eId event
+  modifyPart events $ Map.insert eId event
   forM_
     endCh
     (\ch -> do
-       zoom queue . modify' $ Heap.insert EventKey {ekTime = time, ekId = eId}
-       zoom endIds . modify' $ Map.insert (cell, ch) eId)
+       modifyPart queue $ Heap.insert EventKey {ekTime = time, ekId = eId}
+       modifyPart endIds $ Map.insert (cell, ch) eId)
   return ()
 
 -- | Retrieve the highest priority event from the event generator
-pop :: State EventGen Event
+pop :: (MonadState EventGen m) => m Event
 pop
   -- Pop an identifier from the heap and retrieve the corresponding event
   -- from the hashmap. Then delete the it from the hashmaps.
-  = do
-  eKey <- zoom queue $ state (fromJust . Heap.view)
+ = do
+  eKey <- statePart queue $ fromJust . Heap.view
   let eId = ekId eKey
-  event <-
-    zoom events $ do
-      event <- fmap (Map.! eId) get
-      modify' $ Map.delete eId
-      return event
+  event <- (Map.! eId) <$> gets _events
+  modifyPart events $ Map.delete eId
   forM_
     (view evEndCh event)
-    (\ch -> zoom endIds . modify' $ Map.delete (view evCell event, ch))
+    (\ch -> modifyPart endIds (Map.delete (view evCell event, ch)))
   return event
+
+-- liftState :: (MonadState s ms, MonadState t mt) => ALens' s t -> mt a -> ms a
+-- liftState l m = state $ \s -> case runState m $ s ^# l of
+--                                (a, b) -> (a, s & l #~ b)
+-- | Using a lens, zoom in on a part of the state, apply the monadic action
+-- and return the result.
+statePart :: (MonadState s m) => Lens' s s' -> (s' -> (a, s')) -> m a
+statePart lenss act = do
+  s <- use lenss
+  let (a, s') = act s
+  modify' (set lenss s')
+  return a
+
+-- -- | Using a lens, zoom in on a part of the state, apply the monadic action
+-- -- and return the result.
+-- statePartM :: (MonadState s m) => Lens' s s' -> m (a, s') -> m ()
+-- statePartM lenss act = do
+--   modify' $ over lenss (map snd act)
+--   return ()
+-- | Using a lens, zoom in on a part of the state, apply the state transformer
+modifyPart :: (MonadState s m) => Lens' s s' -> (s' -> s') -> m ()
+modifyPart lenss mod = modify' (over lenss mod)
 
 -- | Correct an event (and its keys) to reflect a channel reassignment
 reassign :: Cell -> Ch -> Ch -> State EventGen ()
@@ -92,60 +166,66 @@ reassign cell fromCh toCh = do
   zoom events . modify' $ Map.adjust (set evEndCh (Just toCh)) eId
   return ()
 
-generateNewEvent :: Double -> Cell -> StateT (StdGen, EventGen) (Reader Opt) ()
+generateNewEvent ::
+     (MonadRandom m, MonadReader Opt m, MonadState EventGen m)
+  => Double
+  -> Cell
+  -> m ()
 generateNewEvent time cell = do
   lam <- asks callRate
-  dt <- zoom _1 (exponentialSt (1.0 / lam :: Double) :: StateT StdGen (Reader Opt) Double)
-  -- TODO NOTE what's up with the return here?
-  _ <- zoom _2 . return $ (push (time + dt) NEW cell Nothing Nothing :: State EventGen ())
+  dt <- sampleRVar (exponential (1 / lam :: Double))
+  _ <- push (time + dt) NEW cell Nothing Nothing
   return ()
 
 generateHoffNewEvent ::
-     Double -> Cell -> Ch -> StateT (StdGen, EventGen) (Reader Opt) ()
+     (MonadRandom m, MonadReader Opt m, MonadState EventGen m)
+  => Double
+  -> Cell
+  -> Ch
+  -> m ()
 generateHoffNewEvent time cell ch = do
   lam <- asks callDurNew
-  -- dt <- zoom _1 $ exponentialSt (lam :: Double)
-  -- let neighs = getNeighs 2 cell False
-  -- neigh_i <- zoom _1 $ state $ randomR (0, length neighs - 1)
-  -- let toCell = neighs !! neigh_i
-  --     t = time + dt
-  (t, toCell) <- zoom _1 $ do
-    dt <- exponentialSt (lam :: Double) :: StateT StdGen (Reader Opt) Double
-    let neighs = getNeighs 2 cell False
-    neigh_i <- state $ randomR (0, length neighs - 1)
-    let toCell = neighs !! neigh_i
-        t = time + dt
-    return (t, toCell)
+  dt <- sampleRVar (exponential (lam :: Double))
+  let neighs = getNeighs 2 cell False
+  neigh_i <- sampleRVar $ uniform 0 (length neighs - 1)
+  let toCell = neighs !! neigh_i
+      t = time + dt
   -- A termination event (END) immediately succeeded
   -- by an arrival event (HOFF) in a neighboring cell
-  -- TODO NOTE what's up with the return here?
-  _ <- zoom _2 . return $ do
-    push t END cell (Just ch) (Just toCell)
-    push t HOFF toCell Nothing Nothing
+  _ <- push t END cell (Just ch) (Just toCell)
+  _ <- push t HOFF toCell Nothing Nothing
+  return ()
+
+_generateEndEvent ::
+     (MonadState EventGen m, MonadRandom m)
+  => Double
+  -> Cell
+  -> Ch
+  -> Double
+  -> m ()
+_generateEndEvent time cell ch lam = do
+  dt <- sampleRVar (exponential (lam :: Double))
+  _ <- push (time + dt) END cell (Just ch) Nothing
   return ()
 
 generateEndEvent ::
-     Double -> Cell -> Ch -> StateT (StdGen, EventGen) (Reader Opt) ()
-generateEndEvent time cell ch = do
-  lam <- asks callDurNew
-  dt <- zoom _1 $ exponentialSt (lam :: Double)
-  -- TODO NOTE what's up with the return here?
-  _ <- zoom _2 . return $ push (time + dt) END cell (Just ch) Nothing
-  return ()
+     (MonadRandom m, MonadReader Opt m, MonadState EventGen m)
+  => Double
+  -> Cell
+  -> Ch
+  -> m ()
+generateEndEvent time cell ch =
+  asks callDurNew >>= _generateEndEvent time cell ch
 
 generateHoffEndEvent ::
-     Double -> Cell -> Ch -> StateT (StdGen, EventGen) (Reader Opt) ()
-generateHoffEndEvent time cell ch = do
-  lam <- asks callDurHoff
-  dt <- zoom _1 $ exponentialSt (lam :: Double)
-  -- TODO NOTE what's up with the return here?
-  _ <- zoom _2 . return $ push (time + dt) END cell (Just ch) Nothing
-  return ()
+     (MonadState EventGen m, MonadReader Opt m, MonadRandom m)
+  => Double
+  -> Cell
+  -> Ch
+  -> m ()
+generateHoffEndEvent time cell ch =
+  asks callDurHoff >>= _generateEndEvent time cell ch
 
 -- | Look up the value for a key 'k'; remove and return the value
 mapRemove :: Ord k => k -> Map.Map k a -> (a, Map.Map k a)
 mapRemove k mapp = (mapp Map.! k, Map.delete k mapp)
-
--- | Given scale parameter 'lambda', sample a double from the exponential distribution
-exponentialSt :: (MonadRandom (State s), MonadState s m) => Double -> m Double
-exponentialSt = state . sampleState . exponential
--- src/Simulator.hs
+++ src/Simulator.hs
@@ -1,89 +1,99 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE Rank2Types #-}
+
 module Simulator where
 
-import Stats
-import Gridfuncs (boolSum2, executeAction, mkGrid, featureRep)
-import EventGen
-import Data.Array.Accelerate (Exp)
-import Control.Monad.State (StateT, state, modify', runStateT, runState)
-import Control.Monad.Reader (Reader, asks)
-import Control.Monad (when, forM_)
-import Data.Maybe (isNothing)
-import System.Random (StdGen, random)
-import Control.Arrow ((&&&))
 import Base
-import Opt
-import Control.Lens (makeLenses, use, zoom, Lens', lens, view, set, _1)
+import Control.Arrow ((&&&))
+import Control.Lens (Lens', lens, makeLenses, over, set, use, view)
+import Control.Monad (when)
+import Control.Monad.Reader (MonadReader, Reader, asks)
+import Control.Monad.State (MonadState, modify', runState)
+import Data.Array.Accelerate (Exp)
+import Data.Maybe (isNothing)
 import Data.Time.Clock (UTCTime)
+import Data.Word (Word64)
+import EventGen (EventGen, mkEventGen, pop)
+import Gridfuncs (featureRep, mkGrid)
+import Opt
+import Stats
+  ( Stats
+  , mkStats
+  , statsEventAcceptNew
+  , statsEventArrivalHoff
+  , statsEventArrivalNew
+  , statsEventEnd
+  , statsEventRejectNew
+  )
 
 data SimState = SimState
-  {
-    _ssGrid :: Grid
+  { _ssGrid :: Grid
   , _ssFrep :: Frep -- Strictly speaking, the frep is not a part of the simulator
   -- The next event for which an action must be selected and executed on 'grid'
   , _ssEvent :: Event
   , _ssEventgen :: EventGen
   , _ssStats :: Stats
-  , _ssGen :: StdGen
   }
 
 makeLenses ''SimState
 
-mkSimState :: UTCTime -> StdGen -> Reader Opt SimState
-mkSimState time gen = do
-  (eg, gen') <- runStateT mkEventGen gen
+mkSimState :: UTCTime -> Word64 -> Reader Opt SimState
+mkSimState time seed = do
+  eg <- mkEventGen seed
   let (event, eg') = runState pop eg :: (Event, EventGen)
   li <- asks Opt.logIter
   let g = mkGrid
       f = featureRep g
-  return $ SimState g f event eg' (mkStats time li) gen'
-
+  return $ SimState g f event eg' (mkStats time li)
 
 -- | Advance the environment 1 step: Log statistics for the event and its corresponding action;
 -- | generate the new event(s); execute the action on the grid and return the resulting reward.
-environmentStep :: Maybe Ch -> StateT SimState (Reader Opt) (Exp Int)
+environmentStep ::
+     (MonadReader Opt m, MonadState SimState m) => Maybe Ch -> m (Exp Int)
 environmentStep act = do
   ev <- use ssEvent
   let eType = view evType ev
-      time = view evTime ev
-      cell = view evCell ev
+      time = view evTime ev :: Double
+      cell = view evCell ev :: Cell
   -- Log call events to Stats record
-  zoom ssStats $ case eType of
+  case eType of
     NEW -> do
-      modify' statsEventArrivalNew
+      modify' $ over ssStats statsEventArrivalNew
       case act of
-        Just _ -> modify' statsEventAcceptNew
-        Nothing -> modify' statsEventRejectNew
+        Just _ -> modify' (over ssStats statsEventAcceptNew)
+        Nothing -> modify' (over ssStats statsEventRejectNew)
     HOFF -> do
-      modify' statsEventArrivalHoff
-      when (isNothing act) $ modify' statsEventRejectNew
-    END -> modify' statsEventEnd
+      modify' (over ssStats statsEventArrivalHoff)
+      when (isNothing act) $ modify' (over ssStats statsEventRejectNew)
+    END -> modify' (over ssStats statsEventEnd)
   -- Generate call events
-  zoom (pairLens2 ssGen ssEventgen) $ case eType of
-    NEW -> do
-      generateNewEvent time cell
-      forM_ act
-        (\ch -> do
-          p <- zoom _1 (state random :: StateT StdGen (Reader Opt) Double)
-          ph <- asks hoffProb
-          if p < ph
-            then generateHoffNewEvent time cell ch
-            else generateEndEvent time cell ch)
-    HOFF -> forM_ act (generateHoffEndEvent time cell)
-    END -> return ()
+  -- modify' $ over ssEventgen (\s -> (runStateT (generateNewEvent time cell) s) >>= put $ (snd) ==> get)
+  -- case eType of
+  --   NEW -> do
+  --     modify' $ zoom ssEventgen (generateNewEvent time cell)
+  --     forM_ act
+  --       (\ch -> do
+  --         -- p <- zoom _1 (state random :: StateT StdGen (Reader Opt) Double)
+  --         p <- sampleRVar (stdUniform)
+  --         ph <- asks hoffProb
+  --         if p < ph
+  --           then generateHoffNewEvent time cell ch
+  --           else generateEndEvent time cell ch)
+  --   HOFF -> forM_ act (generateHoffEndEvent time cell)
+  --   END -> return ()
   -- Execute action, if any, on grid and return call count as reward
-  forM_ act (zoom ssGrid . modify' . executeAction ev)
-  boolSum2 <$> use ssGrid
+  -- forM_ act (zoom ssGrid . modify' . executeAction ev)
+  -- boolSum2 <$> use ssGrid
+  return undefined
 
-
-pairLens2 :: Lens' s x -> Lens' s y -> Lens' s (x,y)
+pairLens2 :: Lens' s x -> Lens' s y -> Lens' s (x, y)
 pairLens2 l1 l2 =
-  lens (view l1 &&& view l2)
-       (\s (x,y) -> set l1 x . set l2 y $ s)
+  lens (view l1 &&& view l2) (\s (x, y) -> set l1 x . set l2 y $ s)
 
-pairLens3 :: Lens' s x -> Lens' s y -> Lens' s z -> Lens' s (x,y,z)
+pairLens3 :: Lens' s x -> Lens' s y -> Lens' s z -> Lens' s (x, y, z)
 pairLens3 l1 l2 l3 =
-  lens (\s -> (view l1 s, view l2 s, view l3 s))
-       (\s (x,y,z) -> set l1 x . set l2 y . set l3 z $ s)
+  lens
+    (\s -> (view l1 s, view l2 s, view l3 s))
+    (\s (x, y, z) -> set l1 x . set l2 y . set l3 z $ s)
